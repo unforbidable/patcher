06d7a259458249e126fe7a7fd57b6dd4bebfb49d
unforbidable <unforbidable@gmail.com>
2015-10-18 12:25:25 +0200

    Misc stats

---
+++ src/Patcher/Data/Plugins/Content/Constants/Skyrim/MiscStat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patcher.Data.Plugins.Content.Constants.Skyrim
+{
+    public enum MiscStat
+    {
+        AnimalsKilled = unchecked((int)0xFCDD5011),
+        ArmorImproved = 0x366D84CF,
+        ArmorMade = 0x023497E6,
+
+        // Misc stat values appear to be some kind of hash produced from the stat name
+    }
+}
