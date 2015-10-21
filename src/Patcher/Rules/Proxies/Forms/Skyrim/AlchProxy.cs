32dd2930b07dd6a1dcee3fb15f95e4fc439ce579
unforbidable <unforbidable@gmail.com>
2015-10-21 14:33:59 +0200

    Fix property

--- src/Patcher/Rules/Proxies/Forms/Skyrim/AlchProxy.cs
+++ src/Patcher/Rules/Proxies/Forms/Skyrim/AlchProxy.cs
@@ -68,7 +68,7 @@ namespace Patcher.Rules.Proxies.Forms.Skyrim
             set
             {
                 EnsureWritable();
-                record.FullName = FullName;
+                record.FullName = value;
             }
         }
 
