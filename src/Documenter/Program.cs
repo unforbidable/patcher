70de26bf0cb5dc4ac572df20af2ca5b68a334e1d
unforbidable <unforbidable@gmail.com>
2015-10-23 10:55:49 +0200

    Add Documenter project

---
+++ src/Documenter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Documenter
+{
+    class Program
+    {
+        public const string RootNamespace = "Patcher.Rules.Compiled";
+        public const string RootFolder = "reference";
+        public const string TargetPath = @"..\..\..\..\doc\generated";
+
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetAssembly(typeof(Patcher.Rules.Compiled.Forms.IForm));
+            var xmlFilePath = Path.GetFileNameWithoutExtension(assembly.GetName().CodeBase) + ".xml";
+
+            XDocument doc = XDocument.Load(xmlFilePath);
+            
+
+            foreach (var type in assembly.GetTypes())
+            {
+                string name = type.GetLocalName();
+                string ns = type.GetLocalNamespace();
+
+                // Ignore extensions (extension methods will be added to the respective class they extend)
+                if (ns.StartsWith("Extensions"))
+                    continue;
+
+                Console.WriteLine("Type {0} in {1}", name, ns);
+
+
+
+            }
+
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Documenter/Utility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documenter
+{
+    static class Utility
+    {
+        public static string GetLocalNamespace(this Type type)
+        {
+            return type.Namespace.Substring(Program.RootNamespace.Length + 1);
+        }
+
+        public static string GetLocalName(this Type type)
+        {
+            if (type.Namespace.Contains(".Helpers"))
+            {
+                return type.Name.TrimStart('I').Replace("Helper", string.Empty);
+            }
+            else if (type.Namespace.Contains(".Forms") || type.Namespace.Contains(".Fields"))
+            {
+                return type.Name.TrimStart('I').Replace("`1", string.Empty);
+            }
+            else
+            {
+                return type.Name;
+            }
+        }
+    }
+}
