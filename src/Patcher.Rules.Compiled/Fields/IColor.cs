a2589b77aeeed68e692e8c422245e932da09e101
unforbidable <unforbidable@gmail.com>
2015-11-05 23:43:43 +0100

    Color proxy

---
+++ src/Patcher.Rules.Compiled/Fields/IColor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patcher.Rules.Compiled.Fields
+{
+    /// <summary>
+    /// Represents a color definition.
+    /// </summary>
+    /// <remarks>
+    /// The valid value range of color components is <code>0.0f</code> through <code>1.0f</code>.
+    /// </remarks>
+    public interface IColor 
+    {
+        /// <summary>
+        /// Gets or sets the red component value.
+        /// </summary>
+        float Red { get; set; }
+        /// <summary>
+        /// Gets or sets the green component value.
+        /// </summary>
+        float Green { get; set; }
+        /// <summary>
+        /// Gets or sets the blue component value.
+        /// </summary>
+        float Blue { get; set; }
+    }
+}
