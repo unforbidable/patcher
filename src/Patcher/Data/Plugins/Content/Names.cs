c8b645d36ec2dd8d68defc72310b7f6e384c9987
unforbidable <unforbidable@gmail.com>
2015-11-11 12:44:22 +0100

    Add dummy Aoru form

--- src/Patcher/Data/Plugins/Content/Names.cs
+++ src/Patcher/Data/Plugins/Content/Names.cs
@@ -31,6 +31,7 @@ namespace Patcher.Data.Plugins.Content
         public const string ACTI = "ACTI";
         public const string ALCH = "ALCH";
         public const string AMMO = "AMMO";
+        public const string AORU = "AORU";
         public const string ARMA = "ARMA";
         public const string ARMO = "ARMO";
         public const string ASPC = "ASPC";
