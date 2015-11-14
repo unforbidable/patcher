74d3a28331acae46ba1c88c5f4f20dd3f82e131c
unforbidable <unforbidable@gmail.com>
2015-11-14 20:38:56 +0100

    IMGS fully implemented

--- src/Patcher.Rules.Compiled/Forms/Fallout4/IImgs.cs
+++ src/Patcher.Rules.Compiled/Forms/Fallout4/IImgs.cs
@@ -23,9 +23,101 @@ using System.Text;
 namespace Patcher.Rules.Compiled.Forms.Fallout4
 {
     /// <summary>
-    /// Represents an <b>Image Space</b> form.  <i>This form is a work in progress.</i>
+    /// Represents an <b>Image Space</b> form.
     /// </summary>
+    /// <remarks>
+    /// The meaning of unknown properties is not known so use them with caution. 
+    /// Unknown properties will be removed and replaced with proper properties as soon as more information is available.
+    /// </remarks>
     public interface IImgs : IForm
     {
+        /// <summary>
+        /// Gets or sets the HDR eye adaptation speed.
+        /// </summary>
+        float EyeAdaptSpeed { get; set; }
+        /// <summary>
+        /// Gets or sets the HDR eye adaptation strength.
+        /// </summary>
+        float EyeAdaptStrength { get; set; }
+        /// <summary>
+        /// Gets or sets the HDR bloom blur radius.
+        /// </summary>
+        float BloomBlurRadius { get; set; }
+        /// <summary>
+        /// Gets or sets the HDR bloom threshold.
+        /// </summary>
+        float BloomThreshold { get; set; }
+        /// <summary>
+        /// Gets or sets the HDR bloom scale.
+        /// </summary>
+        float BloomScale { get; set; }
+        /// <summary>
+        /// Gets or sets the HDR reveive bloom threshold.
+        /// </summary>
+        float ReceiveBloomThreshold { get; set; }
+        /// <summary>
+        /// Gets or sets the HDR white value.
+        /// </summary>
+        float White { get; set; }
+        /// <summary>
+        /// Gets or sets the HDR sunlight scale.
+        /// </summary>
+        float SunlightScale { get; set; }
+        /// <summary>
+        /// Gets or sets the HDR sky scale.
+        /// </summary>
+        float SkyScale { get; set; }
+        /// <summary>
+        /// Gets or sets the cinematic saturation.
+        /// </summary>
+        float Saturation { get; set; }
+        /// <summary>
+        /// Gets or sets the cinematic brightness.
+        /// </summary>
+        float Brightness { get; set; }
+        /// <summary>
+        /// Gets or sets the conematic contrast.
+        /// </summary>
+        float Contrast { get; set; }
+        /// <summary>
+        /// Gets or sets the tint amount.
+        /// </summary>
+        float TintAmount { get; set; }
+        /// <summary>
+        /// Gets or sets the tint color.
+        /// </summary>
+        IColor TintColor { get; set; }
+        /// <summary>
+        /// Gets or sets the Depth of Field strength.
+        /// </summary>
+        float DepthOfFieldStrength { get; set; }
+        /// <summary>
+        /// Gets or sets the Depth of Field distance.
+        /// </summary>
+        float DepthOfFieldDistance { get; set; }
+        /// <summary>
+        /// Gets or sets the Depth of Field range.
+        /// </summary>
+        float DepthOfFieldRange { get; set; }
+        /// <summary>
+        /// Gets or sets the Depth of Field blur radius.
+        /// </summary>
+        float DepthOfFieldBlurRadius { get; set; }
+        /// <summary>
+        /// Gets or sets the value that indicates whether the sky is affected by the Depth of Field effect.
+        /// </summary>
+        bool IsDepthOfFieldSkyDisabled { get; set; }
+        /// <summary>
+        /// Gets or sets an unknown value.
+        /// </summary>
+        float DepthOfFieldUnknown1 { get; set; }
+        /// <summary>
+        /// Gets or sets an unknown value.
+        /// </summary>
+        float DepthOfFieldUnknown2 { get; set; }
+        /// <summary>
+        /// Gets or sets the LUT texture.
+        /// </summary>
+        string LookupTexture { get; set; }
     }
 }
--- src/Patcher/Rules/Proxies/Forms/Fallout4/ImgsProxy.cs
+++ src/Patcher/Rules/Proxies/Forms/Fallout4/ImgsProxy.cs
@@ -28,5 +28,316 @@ namespace Patcher.Rules.Proxies.Forms.Fallout4
     [Proxy(typeof(IImgs))]
     public class ImgsProxy : FormProxy<Imgs>, IImgs
     {
+        public float BloomBlurRadius
+        {
+            get
+            {
+                return record.BloomBlurRadius;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.BloomBlurRadius = value;
+            }
+        }
+
+        public float BloomScale
+        {
+            get
+            {
+                return record.BloomScale;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.BloomScale = value;
+            }
+        }
+
+        public float BloomThreshold
+        {
+            get
+            {
+                return record.BloomThreshold;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.BloomThreshold = value;
+            }
+        }
+
+        public float Brightness
+        {
+            get
+            {
+                return record.Brightness;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.Brightness = value;
+            }
+        }
+
+        public float Contrast
+        {
+            get
+            {
+                return record.Contrast;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.Contrast = value;
+            }
+        }
+
+        public float DepthOfFieldBlurRadius
+        {
+            get
+            {
+                return record.DepthOfFieldBlurRadius;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.DepthOfFieldBlurRadius = value;
+            }
+        }
+
+        public float DepthOfFieldDistance
+        {
+            get
+            {
+                return record.DepthOfFieldDistance;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.DepthOfFieldDistance = value;
+            }
+        }
+
+        public float DepthOfFieldRange
+        {
+            get
+            {
+                return record.DepthOfFieldRange;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.DepthOfFieldRange = value;
+            }
+        }
+
+        public float DepthOfFieldStrength
+        {
+            get
+            {
+                return record.DepthOfFieldStrength;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.DepthOfFieldStrength = value;
+            }
+        }
+
+        public float DepthOfFieldUnknown1
+        {
+            get
+            {
+                return record.DepthOfFieldUnknown1;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.DepthOfFieldUnknown1 = value;
+            }
+        }
+
+        public float DepthOfFieldUnknown2
+        {
+            get
+            {
+                return record.DepthOfFieldUnknown2;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.DepthOfFieldUnknown2 = value;
+            }
+        }
+
+        public float EyeAdaptSpeed
+        {
+            get
+            {
+                return record.EyeAdaptSpeed;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.EyeAdaptSpeed = value;
+            }
+        }
+
+        public float EyeAdaptStrength
+        {
+            get
+            {
+                return record.EyeAdaptStrength;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.EyeAdaptStrength = value;
+            }
+        }
+
+        public bool IsDepthOfFieldSkyDisabled
+        {
+            get
+            {
+                return record.IsDepthOfFieldSkyDisabled;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.IsDepthOfFieldSkyDisabled = value;
+            }
+        }
+
+        public string LookupTexture
+        {
+            get
+            {
+                return record.LookupTexture;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.LookupTexture = value;
+            }
+        }
+
+        public float ReceiveBloomThreshold
+        {
+            get
+            {
+                return record.ReceiveBloomThreshold;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.ReceiveBloomThreshold = value;
+            }
+        }
+
+        public float Saturation
+        {
+            get
+            {
+                return record.Saturation;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.Saturation = value;
+            }
+        }
+
+        public float SkyScale
+        {
+            get
+            {
+                return record.SkyScale;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.SkyScale = value;
+            }
+        }
+
+        public float SunlightScale
+        {
+            get
+            {
+                return record.SunlightScale;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.SunlightScale = value;
+            }
+        }
+
+        public float TintAmount
+        {
+            get
+            {
+                return record.TintAmount;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.TintAmount = value;
+            }
+        }
+
+        public IColor TintColor
+        {
+            get
+            {
+                return Provider.CreateProxy<ColorProxy>(Mode).With(record.TintColor);
+            }
+
+            set
+            {
+                EnsureWritable();
+
+                // Simply copy colors from one adapter to the other
+                record.TintColor.Red = value.Red;
+                record.TintColor.Green = value.Green;
+                record.TintColor.Blue = value.Blue;
+            }
+        }
+
+        public float White
+        {
+            get
+            {
+                return record.White;
+            }
+
+            set
+            {
+                EnsureWritable();
+                record.White = value;
+            }
+        }
     }
 }
